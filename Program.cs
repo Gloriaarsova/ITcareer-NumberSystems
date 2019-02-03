993c3cbe44bca28ab73821e26453f2f353edda26
Gloriaarsova <47273903+Gloriaarsova@users.noreply.github.com>
2019-02-03 16:37:50 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var des = int.Parse(Console.ReadLine());
+            string dv = " ";
+            while(des!=0)
+            {
+                if (des % 2 == 0) dv = '0' + dv;
+                else dv = '1' + dv;
+                des = des / 2;
+            }
+            Console.WriteLine(dv);
+        }
+    }
+}
